88d9deff037dc8fbe3ee483bd205970f66f59dbe
Vibhaj Rajan <vibhaj8@gmail.com>
2011-05-22 01:02:18 +0530

    v0.0.6-final

---
+++ tables.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `uid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`uid`)
+)
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `sessionid` char(32) NOT NULL,
+  `uid` bigint(20) NOT NULL,
+  `time` bigint(20) NOT NULL,
+  `expiry` bigint(20) NOT NULL,
+  PRIMARY KEY (`sessionid`)
+)
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `gid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `gname` varchar(255) NOT NULL,
+  `level` int(11) NOT NULL,
+  PRIMARY KEY (`gid`)
+)
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `gid` bigint(20) NOT NULL,
+  `member` bigint(20) NOT NULL,
+  UNIQUE KEY `gid` (`gid`,`member`),
+  KEY `gid_2` (`gid`)
+)
+
+CREATE TABLE IF NOT EXISTS `logs` (
+  `lid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `time` bigint(20) NOT NULL,
+  PRIMARY KEY (`lid`)
+)
+
+CREATE TABLE IF NOT EXISTS `remarks` (
+  `rkid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL,
+  `comment` text NOT NULL,
+  `rating` int(11) NOT NULL,
+  PRIMARY KEY (`rkid`)
+)
